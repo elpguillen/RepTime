d8b7509a4b66c2c2759ffbe0d03f7b92ef5b867e
elguillen <elguillen@users.noreply.github.com>
2022-05-03 10:12:16 -0700

    Updated layout for fragment_workout and implemented the countdown timer for each repetition in WorkoutFragment.

---
+++ app/src/main/java/com/chiu/reptime/constants.kt
@@ -0,0 +1,11 @@
+package com.chiu.reptime
+
+object constants {
+    val MILLISECONS_IN_SECOND = 1000
+    val SECONDS_IN_MINUTE = 60
+    val MINUTES_IN_HOUR = 60
+
+    val HOUR_TO_SECONDS = MINUTES_IN_HOUR * SECONDS_IN_MINUTE
+
+    val PROGRESS_BAR_FULL = 100
+}
--- app/src/main/java/com/chiu/reptime/fragments/WorkoutFragment.kt
+++ app/src/main/java/com/chiu/reptime/fragments/WorkoutFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.chiu.reptime.constants
 import com.chiu.reptime.databinding.FragmentWorkoutBinding
 import com.chiu.reptime.models.RepTimer
 import com.chiu.reptime.models.RestTimer
@@ -23,7 +24,16 @@ class WorkoutFragment : Fragment() {
 
     private val args: WorkoutFragmentArgs by navArgs()
 
-    private var startTime = 10;
+    // counter to be used in the rep and rest timers
+    private var timeLeft: Long = 0
+
+    // the total time the rep counter will run for
+    private var repTotalTime: Long = 0
+    // the total time the rest counter will run for
+    private var restTotalTime: Long = 0
+
+    private var repCountDownTimer: CountDownTimer? = null
+    private var restCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,16 +55,49 @@ class WorkoutFragment : Fragment() {
         val restTimer = args.restTimer
         val numberReps = args.numberOfReps
 
-        setTimerAndReps(repTimer, restTimer, numberReps)
+        repTotalTime = (repTimeToSeconds(repTimer) * constants.MILLISECONS_IN_SECOND) + constants.MILLISECONS_IN_SECOND
+        restTotalTime = (restTimeToSeconds(restTimer) * constants.MILLISECONS_IN_SECOND) + constants.MILLISECONS_IN_SECOND
 
-        startRepCounter()
+        binding.pbTimer.progress = constants.PROGRESS_BAR_FULL
+
+        if (numberReps > 0) {
+            startRepCounter(repTotalTime, numberReps)
+        } else {
+            defaultDisplay()
+        }
     }
 
-    private fun startRepCounter() {
-        object : CountDownTimer(10000, 1000) {
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    /**
+     * Starts a counter for each repetition.
+     *
+     * @param totalTime the total time the repetition is to go on for
+     * @param numReps the total number of repetitions
+     */
+    private fun startRepCounter(totalTime: Long, numReps: Int) {
+        repCountDownTimer?.cancel()
+
+        binding.remainingRepsLabel.text = numReps.toString()
+
+        timeLeft = totalTime
+
+        repCountDownTimer = object : CountDownTimer(totalTime, constants.MILLISECONS_IN_SECOND.toLong()) {
             override fun onTick(millisUntilFinished: Long) {
-                binding.timerLabel.text = startTime.toString()
-                startTime--
+                // TODO: Have [ProgressBar] change progress based on time elapsed.
+                binding.timerLabel.text = formatTimerDisplay((timeLeft/constants.MILLISECONS_IN_SECOND) - 1)
+                timeLeft -= constants.MILLISECONS_IN_SECOND
+
+                /*
+                 *  This will ensure that once the timer hits '0', the label that updates
+                 *  the number of reps is synchronized with the timer.
+                 */
+                if (timeLeft == 0L && numReps > 0) {
+                    binding.remainingRepsLabel.text = (numReps - 1).toString()
+                }
             }
 
             override fun onFinish() {
@@ -62,34 +105,90 @@ class WorkoutFragment : Fragment() {
                 // how many more reps are left to do:
                 //    if there are more reps to do, start the rest timer
                 //    else stop the timer
-                binding.timerLabel.text = "finished"
+
+                if (numReps > 1) {
+                    /* TODO: when there are still reps to do, then start
+                     *        a rest timer before continuing to do another
+                     *          rep.
+                     */
+                    startRepCounter(repTotalTime, numReps - 1)
+                }
             }
         }.start()
     }
 
-    private fun formatRestTimer(restTimer: RestTimer): String {
+    /*private fun startRestTimer() {
 
-        val minute: String = restTimer.minute.toString()
-        val second: String = restTimer.second.toString()
+        restCountDownTimer?.cancel()
 
-        return "$minute m $second s"
+        restCountDownTimer = object : CountDownTimer(0, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+        }.start()
+    }*/
+
+    /**
+     * Converts the hour(s), minute(s), seconds(s) in [RepTimer] to seconds.
+     *
+     * @param repTimer a timer holding hour(s), minute(s), and second(s)
+     * @return the total number of seconds
+     */
+    private fun repTimeToSeconds(repTimer: RepTimer): Long {
+        val repHour: Long = repTimer.hour.toLong()
+        val repMinute: Long = repTimer.minute.toLong()
+        val repSecond: Long = repTimer.second.toLong()
+
+        return (repHour * constants.MINUTES_IN_HOUR * constants.SECONDS_IN_MINUTE) +
+                (repMinute * constants.SECONDS_IN_MINUTE) + repSecond
     }
 
-    private fun formatRepTimer(repTimer: RepTimer): String {
-        val hour: String = repTimer.hour.toString()
-        val minute: String = repTimer.minute.toString()
-        val second: String = repTimer.second.toString()
+    /**
+     * Converts the minute(s) and second(s) in [RestTimer] to seconds.
+     *
+     * @param restTimer timer holding minute(s) and second(s)
+     * @return the total number of seconds
+     */
+    private fun restTimeToSeconds(restTimer: RestTimer): Long {
+        val restMinute: Long = restTimer.minute.toLong()
+        val restSecond: Long = restTimer.second.toLong()
 
-        return "$hour h $minute m $second s"
+        return (restMinute * constants.SECONDS_IN_MINUTE) + restSecond
     }
 
-    private fun setTimerAndReps(
-        repTimer: RepTimer,
-        restTimer: RestTimer,
-        numReps: Int) {
-        binding.timePerRepLabel.text = formatRepTimer(repTimer)
-        binding.restLabel.text = formatRestTimer(restTimer)
-        binding.numberRepLabel.text = numReps.toString()
+    /**
+     * Converts the number of seconds to a string in the format of:
+     *      "hh:mm:ss"
+     *
+     * @param time the number of seconds
+     * @return "hh:mm:ss"
+     */
+    private fun formatTimerDisplay(time: Long): String {
+        val hour = time / 3600
+        val minutes = (time % 3600) / 60
+        val seconds = (time % 3600) % 60
+
+        if (time < constants.SECONDS_IN_MINUTE)
+            return "$seconds"
+
+        if (time < constants.SECONDS_IN_MINUTE * constants.MINUTES_IN_HOUR)
+            return "$minutes:$seconds"
+
+        return "$hour:$minutes:$seconds"
     }
 
+    /**
+     * Sets the display to 'zero'. This will be the default in the
+     * cases when there is no valid workout to perform.
+     */
+    private fun defaultDisplay() {
+        binding.remainingRepsLabel.text = "0"
+        binding.timerLabel.text = "0"
+        binding.pbTimer.progress = 0
+    }
 }
